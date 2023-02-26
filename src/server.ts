d15b3ea6b872de4ba5736a59caf9e89beb6968fd
Lucas Carioca <lucascarioca97@gmail.com>
2023-02-26 02:58:03 -0300

    feat: implemented routes and tests

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,5 @@
-import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
+import { app } from './app'
 import { env } from './env'
-
-const app = fastify()
-
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
-
-  return transaction
-})
 
 app
   .listen({
